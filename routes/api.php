452e0fb470abff3b0adc5a8d71a51b4d856642e8
ocdigital <eduardo@ocdigital.com.br>
2024-04-09 14:07:50 -0300

    refactoring: eliminating unnecessary AuthUserController class

--- routes/api.php
+++ routes/api.php
@@ -1,32 +1,20 @@
 <?php
 
 use App\Http\Controllers\CardController;
-use App\Http\Controllers\UserAuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExpenseController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('register', [UserAuthController::class, 'register']);
-Route::post('login', [UserAuthController::class, 'login']);
-Route::get('me', [UserAuthController::class, 'me'])->middleware('auth:sanctum');
-Route::post('logout', [UserAuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::apiResource('users', UserController::class)->middleware('auth:sanctum');
+    Route::post('register', [UserController::class, 'register']);
+    Route::post('login', [UserController::class, 'login']);
+    Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::apiResource('users', UserController::class)->only(['index', 'update', 'show', 'destroy'])->middleware('auth:sanctum');
+
 Route::apiResource('cards', CardController::class)->middleware('auth:sanctum');
 Route::apiResource('expenses', ExpenseController::class)->middleware('auth:sanctum');
