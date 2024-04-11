4010b41d8785bf8db507a3998b95d7fbfe853422
ocdigital <eduardo@ocdigital.com.br>
2024-04-10 23:22:52 -0300

    Fix entypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-# Definir permissões na pasta de armazenamento
 chmod -R 777 /var/www/html/storage
 
-# Instalar dependências PHP com o Composer
 composer install
 
-# Copiar o arquivo de exemplo de configuração .env
 cp .env.example .env
 
-# Limpar cache e configurar a aplicação
 php artisan key:generate
 
-
-# Iniciar o Horizon para processamento de filas
 php artisan horizon &
 
-
-# Iniciar o trabalhador da fila de forma assíncrona
 php artisan queue:work --daemon &
 
 php artisan config:clear
 
-# Iniciar o servidor Apache
+php artisan migrate
+
+php artisan db:seed
+
 apache2-foreground
